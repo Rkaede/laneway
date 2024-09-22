94348e15236b34e9c77a89cb16aa79ca3d689f41
coreym <corey.mac@gmail.com>
2024-09-23 09:16:24 +1000

    chore: small refactor of settings page

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,4 +1,4 @@
-import type { ParentComponent } from 'solid-js';
+import type { JSX, ParentComponent } from 'solid-js';
 
 import { MultiCombobox } from '~/components/connected/multi-combobox';
 import { Card, CardContent, Tag } from '~/components/ui';
@@ -21,8 +21,7 @@ import {
 } from '~/components/ui/select';
 import { Switch, SwitchControl, SwitchThumb } from '~/components/ui/switch';
 import { deleteData, setStore, store } from '~/store';
-import { deleteKeys } from '~/store/keys';
-import { apiKeys, setApiKeys } from '~/store/keys';
+import { apiKeys, deleteKeys, setApiKeys } from '~/store/keys';
 import { download } from '~/util';
 
 export function Settings() {
@@ -47,114 +46,89 @@ export function Settings() {
 
 function General() {
   return (
-    <Section>
-      <SectionHeader>
-        <SectionTitle>General</SectionTitle>
-        <SectionDescription>General settings for the app.</SectionDescription>
-      </SectionHeader>
-      <SectionContent>
-        <Setting inline>
-          <SettingHeader>
-            <SettingTitle>Generate Titles</SettingTitle>
-            <SettingDescription>
-              On the first message, automatically generate a title. This requires an OpenAI API
-              key to be set below. The GPT-3.5 Turbo model is used.
-            </SettingDescription>
-          </SettingHeader>
-          <SettingControl>
-            <Switch
-              checked={store.settings.generateTitles}
-              onChange={(value) => setStore('settings', 'generateTitles', value)}
-            >
-              <SwitchControl>
-                <SwitchThumb />
-              </SwitchControl>
-            </Switch>
-          </SettingControl>
-        </Setting>
-        <Setting>
-          <SettingHeader>
-            <SettingTitle>System Model</SettingTitle>
-            <SettingDescription>
-              This model is used by the app to generate title for new sessions.
-            </SettingDescription>
-          </SettingHeader>
-          <MultiCombobox
-            value={{ id: store.settings.systemModel, type: 'model' }}
-            onSelect={(modelId) => setStore('settings', 'systemModel', modelId)}
-            class="w-1/2"
-            includeModels
-          />
-        </Setting>
-        <Setting>
-          <SettingHeader>
-            <SettingTitle>Default Session</SettingTitle>
-            <SettingDescription>The default session for new conversations.</SettingDescription>
-          </SettingHeader>
-          <MultiCombobox
-            value={{
-              id: store.settings.defaultSession.id,
-              type: store.settings.defaultSession.type,
-            }}
-            onSelect={(id, type) => setStore('settings', 'defaultSession', { id, type })}
-            class="w-1/2"
-            includeAssistants
-            includeModels
-            includePresets
-            showType
-          />
-        </Setting>
-      </SectionContent>
+    <Section title="General" description="General settings for the app.">
+      <Setting
+        inline
+        title="Generate Titles"
+        description="On the first message, automatically generate a title. This requires an OpenAI API key to be set below. The GPT-3.5 Turbo model is used."
+      >
+        <SettingControl>
+          <Switch
+            checked={store.settings.generateTitles}
+            onChange={(value) => setStore('settings', 'generateTitles', value)}
+          >
+            <SwitchControl>
+              <SwitchThumb />
+            </SwitchControl>
+          </Switch>
+        </SettingControl>
+      </Setting>
+      <Setting
+        title="System Model"
+        description="This model is used by the app to generate title for new sessions."
+      >
+        <MultiCombobox
+          value={{ id: store.settings.systemModel, type: 'model' }}
+          onSelect={(modelId) => setStore('settings', 'systemModel', modelId)}
+          class="w-1/2"
+          includeModels
+        />
+      </Setting>
+      <Setting title="Default Session" description="The default session for new conversations.">
+        <MultiCombobox
+          value={{
+            id: store.settings.defaultSession.id,
+            type: store.settings.defaultSession.type,
+          }}
+          onSelect={(id, type) => setStore('settings', 'defaultSession', { id, type })}
+          class="w-1/2"
+          includeAssistants
+          includeModels
+          includePresets
+          showType
+        />
+      </Setting>
     </Section>
   );
 }
 
 function Completions() {
   return (
-    <Section>
-      <SectionHeader>
-        <SectionTitle>
+    <Section
+      title={
+        <>
           Completions <Tag variant="warning">Experimental</Tag>
-        </SectionTitle>
-        <SectionDescription>
-          Provides completion suggestions in the chat input field. This is an experimental
-          feature. Please report any issues you encounter.
-        </SectionDescription>
-      </SectionHeader>
-      <SectionContent>
-        <Setting inline>
-          <SettingHeader>
-            <SettingTitle>Enable Completions</SettingTitle>
-            <SettingDescription>
-              Enable or disable completions in the chat input field.
-            </SettingDescription>
-          </SettingHeader>
-          <SettingControl>
-            <Switch
-              checked={store.settings.completions.enabled}
-              onChange={(value) => setStore('settings', 'completions', 'enabled', value)}
-            >
-              <SwitchControl>
-                <SwitchThumb />
-              </SwitchControl>
-            </Switch>
-          </SettingControl>
-        </Setting>
-        <Setting>
-          <SettingHeader>
-            <SettingTitle>Completion Model</SettingTitle>
-            <SettingDescription>
-              This model is used to generate completions in the chat input field.
-            </SettingDescription>
-          </SettingHeader>
-          <MultiCombobox
-            value={{ id: store.settings.completions.model, type: 'model' }}
-            onSelect={(modelId) => setStore('settings', 'completions', 'model', modelId)}
-            class="w-1/2"
-            includeModels
-          />
-        </Setting>
-      </SectionContent>
+        </>
+      }
+      description="Provides completion suggestions in the chat input field. This is an experimental feature. Please report any issues you encounter."
+    >
+      <Setting
+        inline
+        title="Enable Completions"
+        description="Enable or disable completions in the chat input field."
+      >
+        <SettingControl>
+          <Switch
+            checked={store.settings.completions.enabled}
+            onChange={(value) => setStore('settings', 'completions', 'enabled', value)}
+          >
+            <SwitchControl>
+              <SwitchThumb />
+            </SwitchControl>
+          </Switch>
+        </SettingControl>
+      </Setting>
+      <Setting
+        title="Completion Model"
+        description="This model is used to generate completions in the chat input field."
+      >
+        <MultiCombobox
+          value={{ id: store.settings.completions.model, type: 'model' }}
+          onSelect={(modelId) => setStore('settings', 'completions', 'model', modelId)}
+          class="w-1/2"
+          includeModels
+        />
+      </Setting>
     </Section>
   );
 }
@@ -174,169 +148,167 @@ const openRouterOptions: OpenRouterOption[] = [
 
 function APIKeys() {
   return (
-    <Section>
-      <SectionHeader>
-        <SectionTitle>API Keys</SectionTitle>
-        <SectionDescription>
-          Keys are stored locally in your browser and are used to connect to the providers API.
-        </SectionDescription>
-      </SectionHeader>
-      <SectionContent>
-        <Setting>
-          <SettingHeader>
-            <SettingTitle>OpenRouter</SettingTitle>
-            <SettingDescription class="flex flex-col gap-2">
-              <p>
-                Open router bridges AI providers like OpenAI, Anthropic, and Google. Required
-                for Anthropic & Llama models.
-              </p>
-              <p>
-                You can get a key from the{' '}
-                <a href="https://openrouter.ai/keys" target="_blank" rel="noopener noreferrer">
-                  OpenRouter keys page
-                </a>
-                .
-              </p>
-            </SettingDescription>
-          </SettingHeader>
-          <Input
-            data-1p-ignore // disable 1Password extension
-            placeholder="sk-ant-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-            type="password"
-            value={apiKeys?.openrouter ?? ''}
-            onChange={(e) => setApiKeys('openrouter', e.target.value)}
-          />
-
-          <div class="mt-6">
-            <SettingHeader>
-              <SettingDescription>When to use OpenRouter:</SettingDescription>
-            </SettingHeader>
-          </div>
-
-          <Select
-            options={openRouterOptions}
-            optionValue="value"
-            optionTextValue="label"
-            value={openRouterOptions.find((o) => o.value === store.settings.openRouterUsage)}
-            onChange={(value) => value && setStore('settings', 'openRouterUsage', value.value)}
-            itemComponent={(props) => (
-              <SelectItem item={props.item}>{props.item.rawValue.label}</SelectItem>
-            )}
-          >
-            <SelectTrigger>
-              <SelectValue<OpenRouterOption>>
-                {(state) => state.selectedOption().label ?? 'Select option'}
-              </SelectValue>
-            </SelectTrigger>
-            <SelectContent />
-          </Select>
-        </Setting>
-        <Setting>
-          <SettingHeader>
-            <SettingTitle>OpenAI</SettingTitle>
-            <SettingDescription>
-              Used for the ChatGPT models. You can get a key from the{' '}
-              <a
-                href="https://platform.openai.com/api-keys"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                OpenAI dashboard
+    <Section
+      title="API Keys"
+      description="Keys are stored locally in your browser and are used to connect to the providers API."
+    >
+      <Setting
+        title="OpenRouter"
+        description={
+          <>
+            <p>
+              Open router bridges AI providers like OpenAI, Anthropic, and Google. Required for
+              Anthropic & Llama models.
+            </p>
+            <p>
+              You can get a key from the{' '}
+              <a href="https://openrouter.ai/keys" target="_blank" rel="noopener noreferrer">
+                OpenRouter keys page
               </a>
               .
-            </SettingDescription>
-          </SettingHeader>
+            </p>
+          </>
+        }
+      >
+        <Input
+          data-1p-ignore // disable 1Password extension
+          placeholder="sk-ant-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+          type="password"
+          value={apiKeys?.openrouter ?? ''}
+          onChange={(e) => setApiKeys('openrouter', e.target.value)}
+        />
 
-          <Input
-            data-1p-ignore // disable 1Password extension
-            placeholder="sk-or-v1-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-            type="password"
-            value={apiKeys?.openai ?? ''}
-            onChange={(e) => setApiKeys('openai', e.target.value)}
-          />
-        </Setting>
-
-        <Setting>
+        <div class="mt-6">
           <SettingHeader>
-            <SettingTitle>Google</SettingTitle>
-            <SettingDescription>
-              Used for the Gemini models. You can get a key from the{' '}
-              <a
-                href="https://aistudio.google.com/app/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Google AI Studio
-              </a>
-              .
-            </SettingDescription>
+            <SettingDescription>When to use OpenRouter:</SettingDescription>
           </SettingHeader>
-          <Input
-            data-1p-ignore // disable 1Password extension
-            placeholder="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-            type="password"
-            value={apiKeys?.google ?? ''}
-            onChange={(e) => setApiKeys('google', e.target.value)}
-          />
-        </Setting>
-      </SectionContent>
+        </div>
+
+        <Select
+          options={openRouterOptions}
+          optionValue="value"
+          optionTextValue="label"
+          value={openRouterOptions.find((o) => o.value === store.settings.openRouterUsage)}
+          onChange={(value) => value && setStore('settings', 'openRouterUsage', value.value)}
+          itemComponent={(props) => (
+            <SelectItem item={props.item}>{props.item.rawValue.label}</SelectItem>
+          )}
+        >
+          <SelectTrigger>
+            <SelectValue<OpenRouterOption>>
+              {(state) => state.selectedOption().label ?? 'Select option'}
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent />
+        </Select>
+      </Setting>
+      <Setting
+        title="OpenAI"
+        description={
+          <>
+            Used for the ChatGPT models. You can get a key from the{' '}
+            <a
+              href="https://platform.openai.com/api-keys"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              OpenAI dashboard
+            </a>
+            .
+          </>
+        }
+      >
+        <Input
+          data-1p-ignore // disable 1Password extension
+          placeholder="sk-or-v1-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+          type="password"
+          value={apiKeys?.openai ?? ''}
+          onChange={(e) => setApiKeys('openai', e.target.value)}
+        />
+      </Setting>
+
+      <Setting
+        title="Google"
+        description={
+          <>
+            Used for the Gemini models. You can get a key from the{' '}
+            <a
+              href="https://aistudio.google.com/app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Google AI Studio
+            </a>
+            .
+          </>
+        }
+      >
+        <Input
+          data-1p-ignore // disable 1Password extension
+          placeholder="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+          type="password"
+          value={apiKeys?.google ?? ''}
+          onChange={(e) => setApiKeys('google', e.target.value)}
+        />
+      </Setting>
     </Section>
   );
 }
 
 function Data() {
   return (
-    <Section>
-      <SectionHeader>
-        <SectionTitle>Data</SectionTitle>
-        <SectionDescription>All data is stored locally in your browser.</SectionDescription>
-      </SectionHeader>
-      <SectionContent>
-        <Setting inline>
-          <SettingHeader>
-            <SettingTitle>Delete Data</SettingTitle>
-            <SettingDescription>
-              Removes all data from your browser except for the API keys.
-            </SettingDescription>
-          </SettingHeader>
-          <div class="flex justify-end">
-            <Button variant="destructive" onClick={() => deleteData()}>
-              Delete Data
-            </Button>
-          </div>
-        </Setting>
-        <Setting inline>
-          <SettingHeader>
-            <SettingTitle>Delete API Keys</SettingTitle>
-            <SettingDescription>Removes the API keys from your browser.</SettingDescription>
-          </SettingHeader>
-          <div class="flex justify-end">
-            <Button variant="destructive" onClick={() => deleteKeys()}>
-              Delete Keys
-            </Button>
-          </div>
-        </Setting>
-        <Setting inline>
-          <div class="flex flex-1 flex-col gap-2">
-            <SettingTitle>Export Data</SettingTitle>
-            <SettingDescription>Backup all your data to a file.</SettingDescription>
-          </div>
-          <div class="flex justify-end">
-            <Button
-              variant="default"
-              onClick={() => download(JSON.stringify(store), 'chat-data.json')}
-            >
-              Export Data
-            </Button>
-          </div>
-        </Setting>
-      </SectionContent>
+    <Section title="Data" description="All data is stored locally in your browser.">
+      <Setting
+        inline
+        title="Delete Data"
+        description="Removes all data from your browser except for the API keys."
+      >
+        <div class="flex justify-end">
+          <Button variant="destructive" onClick={() => deleteData()}>
+            Delete Data
+          </Button>
+        </div>
+      </Setting>
+      <Setting
+        inline
+        title="Delete API Keys"
+        description="Removes the API keys from your browser."
+      >
+        <div class="flex justify-end">
+          <Button variant="destructive" onClick={() => deleteKeys()}>
+            Delete Keys
+          </Button>
+        </div>
+      </Setting>
+      <Setting inline title="Export Data" description="Backup all your data to a file.">
+        <div class="flex justify-end">
+          <Button
+            variant="default"
+            onClick={() => download(JSON.stringify(store), 'chat-data.json')}
+          >
+            Export Data
+          </Button>
+        </div>
+      </Setting>
     </Section>
   );
 }
 
-const Section: ParentComponent = (props) => {
-  return <div class="container mx-auto flex flex-col gap-1">{props.children}</div>;
+type SectionProps = {
+  title?: JSX.Element;
+  description?: JSX.Element;
+};
+
+const Section: ParentComponent<SectionProps> = (props) => {
+  return (
+    <div class="container mx-auto flex flex-col gap-1">
+      <div class="pb-2 pl-6">
+        <SectionTitle>{props.title}</SectionTitle>
+        <SectionDescription>{props.description}</SectionDescription>
+      </div>
+      <SectionContent>{props.children}</SectionContent>
+    </div>
+  );
 };
 
 const SectionContent: ParentComponent = (props) => (
@@ -349,11 +321,13 @@ const SettingControl: ParentComponent = (props) => {
   return <div class="flex justify-end">{props.children}</div>;
 };
 
-const SectionHeader: ParentComponent = (props) => {
-  return <div class="pb-2 pl-6">{props.children}</div>;
+type SettingProps = {
+  inline?: boolean;
+  title?: string;
+  description?: JSX.Element;
 };
 
-const Setting: ParentComponent<{ inline?: boolean }> = (props) => {
+const Setting: ParentComponent<SettingProps> = (props) => {
   return (
     <div
       classList={{
@@ -361,6 +335,10 @@ const Setting: ParentComponent<{ inline?: boolean }> = (props) => {
         'flex flex-col gap-1': !props.inline,
       }}
     >
+      <SettingHeader>
+        <SettingTitle>{props.title}</SettingTitle>
+        <SettingDescription>{props.description}</SettingDescription>
+      </SettingHeader>
       {props.children}
     </div>
   );
